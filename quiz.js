d90a6ccd002a07e1bc5c10707678672e528d12a2
Mike Meadows <mmeadow33@gmail.com>
2016-06-05 20:46:52 -0500

    final

--- quiz.js
+++ quiz.js
@@ -1,21 +1,40 @@
 
-var input1 = document.getElementById("enter").addEventListener("click", function(){
-  var treeHeight = document.getElementById("height").value;
-  tree(treeHeight)
-});
+var tree = { };
 
+var Height = document.getElementById("height");
+var Symbol = document.getElementById("symbol");
 
-var input2 = document.getElementById("enter").addEventListener("click", function(){
-  var treeSymbol = document.getElementById("symbol").value;
-  tree(treeSymbol)
-});
+var newTree = document.getElementById('build');
+build.addEventListener('click', makeTree);
 
-function tree (input1, input2) {
-for (var i = 0; i <= input1; i++) {
-	console.log(input2);
-}
+height.addEventListener('keypress', keypressEvent);
+symbol.addEventListener('keypress', keypressEvent);
+
+function keypressEvent (event) {
+  if ( event.keyCode === 13 ) {
+    makeTree();
+  }; 
 };
 
+ function makeTree() {
+  tree.height = height.value;
+  tree.sym = symbol.value;
+	handleGrowTree(tree);
+};
 
+function handleGrowTree(tree) {
 
+  var spaces;
+  var spaceCounter = tree.height - 1;
 
+  var character;
+  var characterCounter = 1;
+  for (var i = 0; i < tree.height; i++) {
+    spaces = ' '.repeat(spaceCounter);
+    character = tree.sym.repeat(characterCounter);
+    spaceCounter--
+    characterCounter = characterCounter + 2;
+
+    console.log(spaces + character);
+};
+};
